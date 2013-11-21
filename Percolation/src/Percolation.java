f39f28fbb9cfa7bedbab4dd52a78ce5efc06c05a
keithmikoleit <keith.mikoleit@gmail.com>
2013-11-20 16:44:26 -0800

    Percolation Checkin

---
+++ Percolation/src/Percolation.java
@@ -0,0 +1,31 @@
+
+public class Percolation {
+	
+		// Data
+		int N; // Stores number of rows and columsn
+	
+		// create N-by-N grid, with all sites blocked
+	   	public Percolation(int N){
+	   		
+	   	}
+	   	
+	 // open site (row i, column j) if it is not already
+	   	public void open(int i, int j){
+	   		
+	   	}
+	   	
+	 // is site (row i, column j) open?
+	   	public boolean isOpen(int i, int j){
+	   		
+	   	}
+	   	
+	 // is site (row i, column j) full?
+	   	public boolean isFull(int i, int j){
+	   		
+	   	}
+	   	
+	 // does the system percolate?
+	   	public boolean percolates(){
+	   		
+	   	}
+}
